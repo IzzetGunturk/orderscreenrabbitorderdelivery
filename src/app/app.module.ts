1182770f7e6e0c4d17a51e46a3e440263cf7e5b7
IzzetGunturk <izzetgunturk2@hotmail.com>
2024-05-23 10:11:52 +0200

    Orders are dynamic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +19,8 @@ import { SvgiconsComponent } from './svgicons/svgicons.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule, // for ngModel
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/kitchenstaff/kitchenstaff.component.ts
+++ src/app/kitchenstaff/kitchenstaff.component.ts
@@ -1,10 +1,39 @@
+// kitchenstaff.component.ts
 import { Component } from '@angular/core';
+
+interface Order {
+  icon: string;
+  id: number;
+  type: string;
+  dishes: Dish[];
+}
+
+interface Dish {
+  name: string;
+  quantity: number;
+  prepared: boolean;
+}
 
 @Component({
   selector: 'app-kitchenstaff',
   templateUrl: './kitchenstaff.component.html',
-  styleUrl: './kitchenstaff.component.scss'
+  styleUrls: ['./kitchenstaff.component.scss']
 })
 export class KitchenstaffComponent {
+  orders: Order[] = [
+    {
+      icon: 'check',
+      id: 1234,
+      type: 'pickup',
+      dishes: [
+        { name: 'Wrap Crispy Chicken', quantity: 1, prepared: false },
+        { name: '5Kitchens Salade', quantity: 1, prepared: false },
+        { name: 'Fanta', quantity: 1, prepared: false }
+      ]
+    },
+  ];
 
+  togglePreparedStatus(dishes: Dish) {
+    dishes.prepared = !dishes.prepared;
+  }
 }
