559d67e8a6f4c3d47b9ce1cef230523a86ec0e04
IzzetGunturk <izzetgunturk2@hotmail.com>
2024-05-23 16:43:59 +0200

    begin to pickupscreen

--- src/app/pickup/pickup.component.ts
+++ src/app/pickup/pickup.component.ts
@@ -1,10 +1,111 @@
 import { Component } from '@angular/core';
 
+interface Order {
+  icon: string;
+  id: number;
+  type: string;
+  comment: string;
+  dishes: Dish[];
+}
+
+interface Dish {
+  name: string;
+  option: string;
+  quantity: number;
+  prepared: boolean;
+}
 @Component({
   selector: 'app-pickup',
   templateUrl: './pickup.component.html',
   styleUrl: './pickup.component.scss'
 })
-export class PickupComponent {
 
+export class PickupComponent {
+  orders: Order[] = [
+    {
+      icon: 'pickup',
+      id: 1238,
+      type: 'Pick up',
+      comment: '',
+      dishes: [
+        { name: 'Wrap Crispy Chicken', option: '', quantity: 1, prepared: false },
+        { name: '5Kitchens Salade', option: '', quantity: 1, prepared: false },
+        { name: 'Fanta', quantity: 1, option: '', prepared: false }
+      ]
+    },
+    {
+      icon: 'table',
+      id: 1239,
+      type: '3',
+      comment: '',
+      dishes: [
+        { name: 'Pasta Bolognese', option: 'Penne', quantity: 1, prepared: false },
+        { name: 'Pepsi Regular', option: '', quantity: 1, prepared: false },
+      ]
+    },
+    {
+      icon: 'table',
+      id: 1240,
+      type: '6',
+      comment: '',
+      dishes: [
+        { name: "Oma's Chili", option: 'Zonder tomaat', quantity: 1, prepared: false },
+        { name: "Fanta", option: '', quantity: 1, prepared: false },
+      ]
+    },
+    {
+      icon: 'pickup',
+      id: 1241,
+      type: 'Pick up',
+      comment: 'Pizza niet snijden',
+      dishes: [
+        { name: 'Pizza Tonno', option: '', quantity: 1, prepared: false },
+        { name: 'Pasta Bolognese', option: 'Tagliatelle', quantity: 1, prepared: false },
+      ]
+    },
+    {
+      icon: 'delivery',
+      id: 1242,
+      type: 'Delivery',
+      comment: 'Graag rietjes erbij',
+      dishes: [
+        { name: 'Zalmfilet', option: 'Witte Wijnsaus', quantity: 1, prepared: false },
+        { name: '5Kitchens Salade', option: '', quantity: 1, prepared: false },
+        { name: 'Classic Burger', option: '', quantity: 1, prepared: false },
+      ]
+    },
+    {
+      icon: 'delivery',
+      id: 1242,
+      type: 'Delivery',
+      comment: 'Graag rietjes erbij',
+      dishes: [
+        { name: 'Zalmfilet', option: 'Witte Wijnsaus', quantity: 1, prepared: false },
+        { name: '5Kitchens Salade', option: '', quantity: 1, prepared: false },
+        { name: 'Classic Burger', option: '', quantity: 1, prepared: false },
+      ]
+    },
+  ];
+  
+  // mark order as done + timeout effect
+  markOrderAsPrepared(index: number) {
+    if (index < this.orders.length && index > -1) {
+      const order = document.querySelectorAll('.order')[index];
+  
+      if (order) {
+        order.classList.add('fade-out');
+      }
+  
+      setTimeout(() => {
+        this.orders.splice(index, 1);
+      }, 2000);
+    }
+  }
+
+  // date time
+  currentDateTime: Date;
+  
+  constructor() {
+    this.currentDateTime = new Date();
+  }
 }
